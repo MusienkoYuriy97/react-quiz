ef110c7c93e44fdfbe57cc768c7d7f8cb8c8ffbe
MusienkoYuriy97 <97musienko@gmail.com>
2022-10-05 13:58:14 +0300

    Ninth foulder - auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import Button from '../../components/ui/Button/Button';
+import Input from '../../components/ui/Input/Input';
+import classes from './Auth.css';
 
 export default class Auth extends Component {
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    handleLogin = () => {
+
+    }
+
+    handleRegistration = () => {
+
+    }
+
     render() {
         return (
-            <div>
-                Auth
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.handleSubmit} className={classes.AuthForm}>
+                        <Input label="Email" />
+                        <Input label="Пароль" errorMessage='TEST' />
+                        <Button type="success" onClick={this.handleLogin}>Войти</Button>
+                        <Button type="primary" onClick={this.handleRegistration}>Регистрация</Button>
+                    </form>
+                </div>
             </div>
         )
     }
